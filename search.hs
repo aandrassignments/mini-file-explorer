20b23694a1a16801c7d948275ebe11f33acfe574
Ibrahim <ibrahimfaris636@gmail.com>
2026-07-05 08:36:18 +0700

    commit

--- search.hs
+++ search.hs
@@ -1,27 +1,140 @@
-import System.Directory (listDirectory, doesDirectoryExist)
-import System.FilePath ((</>))
-import Data.List (isInfixOf)
+import System.Directory
+import System.FilePath
+import Data.Char
+import Data.List
 
-scan :: FilePath -> IO[FilePath]
-scan path = do
-  items <- listDirectory path
-  results <- mapM (process path) items --traverse works too
-  return (concat results)
+-----------------------------------------
+-- Mengambil seluruh file dalam folder
+-----------------------------------------
 
-process :: FilePath -> FilePath -> IO[FilePath] 
-process path item = do
-  let fullPath = path </> item
-  isDir <- doesDirectoryExist fullPath
-  if isDir
-    then do
-      subPath <- scan fullPath
-      return (fullPath : subPath)
-    else return [fullPath]
+scanFolder :: FilePath -> IO [FilePath]
+scanFolder path = do
+    items <- listDirectory path
+    processItems path items
+
+processItems :: FilePath -> [FilePath] -> IO [FilePath]
+processItems _ [] = return []
+
+processItems path (x:xs) = do
+    let full = path </> x
+    isDir <- doesDirectoryExist full
+
+    rest <- processItems path xs
+
+    if isDir
+        then do
+            inside <- scanFolder full
+            return (inside ++ rest)
+        else
+            return (full : rest)
+
+-----------------------------------------
+-- Membaca isi file
+-----------------------------------------
+
+readDocument :: FilePath -> IO (FilePath,String)
+readDocument file = do
+    content <- readFile file
+    return (file,map toLower content)
+
+-----------------------------------------
+-- Membuat index
+-----------------------------------------
+
+buildIndex :: [FilePath] -> IO [(FilePath,String)]
+buildIndex files = mapM readDocument files
+
+-----------------------------------------
+-- Search
+-----------------------------------------
+
+searchDocs :: String -> [(FilePath,String)] -> [FilePath]
+searchDocs keyword docs =
+    map fst $
+    filter contains docs
+    where
+        key = map toLower keyword
+
+        contains (_,content) =
+            isInfixOf key content
+
+-----------------------------------------
+-- Statistik
+-----------------------------------------
+
+countWords :: [(FilePath,String)] -> Int
+countWords docs =
+    foldl (+) 0 $
+    map wordCount docs
+    where
+        wordCount (_,txt) =
+            length (words txt)
+
+-----------------------------------------
+-- Menu
+-----------------------------------------
+
+menu :: [(FilePath,String)] -> IO ()
+
+menu docs = do
+
+    putStrLn "\n===== MINI SEARCH ENGINE ====="
+    putStrLn "1. Cari Kata"
+    putStrLn "2. Jumlah Dokumen"
+    putStrLn "3. Total Kata"
+    putStrLn "4. Keluar"
+
+    putStr "Pilihan : "
+
+    choice <- getLine
+
+    case choice of
+
+        "1" -> do
+            putStr "Masukkan keyword : "
+            key <- getLine
+
+            let result = searchDocs key docs
+
+            if null result
+                then putStrLn "\nTidak ditemukan."
+                else do
+                    putStrLn "\nHasil:"
+                    mapM_ putStrLn result
+
+            menu docs
+
+        "2" -> do
+            putStrLn $
+                "\nJumlah dokumen : "
+                ++ show (length docs)
+
+            menu docs
+
+        "3" -> do
+            putStrLn $
+                "\nTotal kata : "
+                ++ show (countWords docs)
+
+            menu docs
+
+        "4" -> do
+            putStrLn "Terima kasih."
+
+        _ -> do
+            putStrLn "Pilihan salah."
+            menu docs
+
+-----------------------------------------
+-- Main
+-----------------------------------------
+
+main :: IO ()
 
 main = do
-  index <- scan "."
-  putStrLn "\nSearch Something Here"
-  query <- getLine
-  let results = filter (isInfixOf query) index
-  putStrLn "\nSearch Result :"
-  mapM_ putStrLn results
+
+    files <- scanFolder "documents"
+
+    index <- buildIndex files
+
+    menu index
